1e96b4747af19315317db80d65247bdf66e71ac6
Roxanacayul <roxanacayul@gmail.com>
2021-01-09 13:52:00 -0300

    jeje

--- src/kernels.cuh
+++ src/kernels.cuh
@@ -1,5 +1,7 @@
 #pragma once
 #include <cuda.h>
+#include "defines.h"
+
 
 __device__ REAL computeNexta(REAL *a, REAL *F, REAL *G, size_t t, size_t tm1, size_t tm2, size_t r, size_t theta, size_t phi, size_t M, size_t N, size_t O, REAL dt, REAL dr, REAL dtheta, REAL dphi, REAL l_1, REAL l_2, REAL L);
 __device__ REAL computeNextF(REAL *a, REAL *F, REAL *G, size_t t, size_t tm1, size_t tm2, size_t r, size_t theta, size_t phi, size_t M, size_t N, size_t O, REAL dt, REAL dr, REAL dtheta, REAL dphi, REAL l_1, REAL l_2, REAL L);
