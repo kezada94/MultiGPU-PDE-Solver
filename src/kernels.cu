58412c76b1c760d2329b1adb4cc3f19d078ee02c
Felipe Quezada <kezada66@gmail.com>
2021-01-14 18:26:13 -0300

    assa

--- src/kernels.cu
+++ src/kernels.cu
@@ -3,7 +3,9 @@
 #include <cuda.h>
 
 
-#include "Equation.cuh"
+#include "EquationAlfa.cuh"
+#include "EquationF.cuh"
+#include "EquationAlfa.cuh"
 
 __global__ void computeNextIteration(REAL* a, REAL* F, REAL *G, size_t l, size_t t, size_t tm1, size_t tm2, size_t tm3, size_t M, size_t N, size_t O, REAL dt, REAL dr, REAL dtheta, REAL dphi, REAL l_1, REAL l_2, REAL bigl, int p, int q){
 
