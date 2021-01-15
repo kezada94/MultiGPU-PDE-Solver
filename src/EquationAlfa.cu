b3249bee67629ee6f9d12bf0eb7c44a38928619f
Felipe Quezada <kezada66@gmail.com>
2021-01-15 20:52:55 -0300

    dsa

--- src/EquationAlfa.cu
+++ src/EquationAlfa.cu
@@ -1,7 +1,7 @@
 #include "EquationAlfa.cuh"
 
 
-__global__ void computeNexta(REAL *a, REAL *F, REAL *G, size_t t, size_t tm1, size_t tm2, size_t r, size_t theta, size_t phi, size_t M, size_t N, size_t O, REAL dt, REAL dr, REAL dtheta, REAL dphi, REAL l_1, REAL l_2, REAL L)
+__global__ void computeNexta(REAL *a, REAL *F, REAL *G, size_t t, size_t tm1, size_t tm2, size_t M, size_t N, size_t O, REAL dt, REAL dr, REAL dtheta, REAL dphi, REAL l_1, REAL l_2, REAL L)
 {
     int m = blockIdx.x*blockDim.x + threadIdx.x;
 	int n = blockIdx.y*blockDim.y + threadIdx.y;
