47345ed3fdd81d3ebf5b0f4f704287dbdcd4a321
Felipe Quezada <kezada66@gmail.com>
2021-01-19 23:30:54 -0300

    cs

--- gpu-long/src/EquationAlfa.cuh
+++ gpu-long/src/EquationAlfa.cuh
@@ -9,4 +9,4 @@
 #include "defines.h"
 using namespace std;
 
-__global__ void computeNexta(REAL *a, REAL *F, REAL *G, size_t t, size_t tm1, size_t tm2, size_t r, size_t theta, size_t phi, size_t M, size_t N, size_t O, REAL dt, REAL dr, REAL dtheta, REAL dphi, REAL l_1, REAL l_2, REAL L);
+__global__ void computeNexta(REAL *a, REAL *F, REAL *G, size_t t, size_t tm1, size_t tm2, size_t M, size_t N, size_t O, REAL dt, REAL dr, REAL dtheta, REAL dphi, REAL l_1, REAL l_2, REAL L);
