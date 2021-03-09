6723151167107ad80b2c65245b1394fb5a7eeb63
Felipe Quezada <kezada66@gmail.com>
2021-03-09 18:34:13 -0300

    asd

--- Graph2.py
+++ Graph2.py
@@ -28,8 +28,8 @@ print(Z.shape)
 
 data = Z[0, 1:, 1:, -2:]
 interr = (data - (data.min()))
-print("T00 maximo:", data.max())
-print("T00 minimo:", data.min())
+#print("T00 maximo:", data.max())
+#print("T00 minimo:", data.min())
 scale = (interr )#np.clip(interr, 0, interr.max())
 #positive = (np.clip(scale, 0.0000, scale.max()))#np.clip(interr, 0, interr.max())
 #negative = (np.clip(-scale, 0.0000, -scale.min()))
