6a667324626edb3cf1ad88e224cd0e408730ed29
Felipe Quezada <kezada66@gmail.com>
2021-03-22 12:42:19 -0300

    asd

--- cpu/src/defines.h
+++ cpu/src/defines.h
@@ -2,8 +2,8 @@
 #include <iostream>
 
 #define GHOST_SIZE 2
-#define I(t, r, theta, phi) (t)*(M+GHOST_SIZE-1)*(N+GHOST_SIZE-1)*(O+GHOST_SIZE-1) + (r)*(N+GHOST_SIZE-1)*(O+GHOST_SIZE-1) + (theta)*(O+GHOST_SIZE-1) + phi
-#define E(t, r, theta, phi) (t)*(M)*(N)*(O) + (r)*(N)*(O) + (theta)*(O) + phi
+#define I(t, r, theta, phi) (t)*(M+GHOST_SIZE)*(N+GHOST_SIZE)*(O+GHOST_SIZE) + (r+1)*(N+GHOST_SIZE)*(O+GHOST_SIZE) + (theta)*(O+GHOST_SIZE) + phi
+#define E(t, r, theta, phi) (t)*(M+GHOST_SIZE)*(N)*(O) + (r)*(N)*(O) + (theta)*(O) + phi
 
 #define E1 0.001
 #define E2 0.001
@@ -15,4 +15,5 @@
 #define PI_1 0//E1*sin(dr*m)*sin(dtheta*n)*sin(dphi*o)
 #define PI_2 0//E2*sin(dr*m)*sin(dtheta*n)*sin(dphi*o)
 #define PI_3 0//E3*sin(dr*m)*sin(dtheta*n)*sin(dphi*o)
+
 typedef double REAL;
