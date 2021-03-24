6c658b1136ff37ec2a9a4b90ee962d90362fb9d4
Felipe Quezada <felipe.quezada01@outlook.com>
2021-03-24 18:40:50 -0300

    fixed gpu

--- cpu/src/kernels.cpp
+++ cpu/src/kernels.cpp
@@ -91,7 +91,7 @@ void computeFirstIteration(REAL* a, REAL* F, REAL *G, size_t l, size_t t, size_t
 
 } 
 
-void computeFirstIteration(REAL* a, REAL* F, REAL *G, size_t l, size_t t, size_t tm1, size_t tm2, size_t tm3, size_t M, size_t N, size_t O, REAL dt, REAL dr, REAL dtheta, REAL dphi, REAL l_1, REAL l_2, REAL lamb, int p, int q, int L, REAL* a_0){
+void computeSecondIteration(REAL* a, REAL* F, REAL *G, size_t l, size_t t, size_t tm1, size_t tm2, size_t tm3, size_t M, size_t N, size_t O, REAL dt, REAL dr, REAL dtheta, REAL dphi, REAL l_1, REAL l_2, REAL lamb, int p, int q, int L, REAL* a_0){
 	
 	#pragma omp parallel for schedule(dynamic) num_threads(10)
 	for(size_t r=0; r<M; r++){
