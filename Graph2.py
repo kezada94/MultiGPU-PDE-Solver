cc7df5cea2885ff95724b3a6cd287565c0291b7e
Felipe Quezada <kezada66@gmail.com>
2021-04-26 17:00:56 -0400

    dd

--- Graph2.py
+++ Graph2.py
@@ -21,15 +21,27 @@ n = sys.argv[2]
 q = sys.argv[3]
 t = int(sys.argv[4])
 _M = str(M)
-Z = np.genfromtxt("result2-"+str(n)+"-"+str(q)+"-"+_M+".dat", delimiter="\n")
-Z = Z.reshape((-1, 100, 100, 15))
+if (t==0):
+    Z = np.genfromtxt("result-"+str(n)+"-"+str(q)+"-"+_M+"-A.dat", delimiter="\n")
+elif t==1:
+    Z = np.genfromtxt("result-"+str(n)+"-"+str(q)+"-"+_M+"-F.dat", delimiter="\n")
+else:
+    Z = np.genfromtxt("result-"+str(n)+"-"+str(q)+"-"+_M+"-G.dat", delimiter="\n")
+
+
+Z = Z.reshape((-1, 100, 100, 10))
 
 L = Z.shape[0]
 print(Z.shape)
 
-data = Z[t, :, :, :]
+
+l = 0
+data = Z[l, :, :, :]
+print(data.shape)
+
+
 interr = (data - (data.min()))
-print("T00 maximo:", data.max())
+print("Valor maximo:", data.max())
 print("T00 minimo:", data.min())
 scale = (interr )#np.clip(interr, 0, interr.max())
 positive = (np.clip(scale, 0.0000, scale.max()))#np.clip(interr, 0, interr.max())
@@ -40,7 +52,7 @@ d2 = np.empty(data.shape + (4,), dtype=np.ubyte)
 d2[..., 0] = scale/scale.max() * (255.)
 d2[..., 1] = scale/scale.max() * (255.)
 d2[..., 2] = 255# - scale/scale.max() * (255.)
-d2[..., 3] = 20
+d2[..., 3] = 30
 d2[:, 0, 0] = [255,0,0,250]
 d2[0, :, 0] = [0,255,0,250]
 d2[0, 0, :] = [0,0,255,250]
@@ -78,6 +90,50 @@ w.addItem(v)
 
 ax = gl.GLAxisItem()
 w.addItem(ax)
+
+
+def update():
+    global v, d2, l, Z, t
+    ## update volume colors
+    data = Z[l, :, :, :]
+    interr = (data - (data.min()))
+    print("Valor maximo:", data.max())
+    print("T00 minimo:", data.min())
+    scale = (interr )#np.clip(interr, 0, interr.max())
+    positive = (np.clip(scale, 0.0000, scale.max()))#np.clip(interr, 0, interr.max())
+    negative = (np.clip(-scale, 0.0000, -scale.min()))
+    d2 = np.empty(data.shape + (4,), dtype=np.ubyte)
+    #d2[..., 0] = positive/positive.max() * (255.)
+    #d2[..., 1] = negative/negative.max() * (255.)
+    d2[..., 0] = scale/scale.max() * (255.)
+    d2[..., 1] = scale/scale.max() * (255.)
+    d2[..., 2] = 255# - scale/scale.max() * (255.)
+    d2[..., 3] = 30
+
+
+    err = 0
+    dj = np.pi/(100.0-1.0)
+    dk = (2*np.pi)/(10.0-1.0)
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            for k in range(data.shape[2]):
+                if t==1:
+                    err += (1.0*(dj*(j)) - data[i, j, k])**2
+                elif t==2:
+                    err += (25.0*(0.00001*(l) - dk*(k)) - data[i, j, k])**2
+    print(err)
+    
+
+    v.setData(d2)
+    l = (l+1)
+    if (l == Z.shape[0]):
+        print("REINICIOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!!")
+        l=0
+
+pop = QtCore.QTimer()
+pop.timeout.connect(update)
+pop.start(100)
+
 ## Start Qt event loop unless running in interactive mode.
 if __name__ == '__main__':
     import sys
