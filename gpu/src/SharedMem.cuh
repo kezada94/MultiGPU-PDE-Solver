abb2f6f4d90693d28f28ca881ca697973e6f8f00
Felipe Quezada <kezada66@gmail.com>
2021-06-17 14:06:09 -0400

    2nd try

--- gpu/src/SharedMem.cuh
+++ gpu/src/SharedMem.cuh
@@ -1,6 +1,6 @@
 #include "defines.h"
 
-__device__ fillSharedMemory(REAL* sh_a, REAL* sh_F, REAL* sh_G, REAL *a, REAL *F, REAL *G, size_t M, size_t N, size_t O, size_t global_phi, int r, int theta, int phi){
+__device__ void fillSharedMemory(REAL* sh_a, REAL* sh_F, REAL* sh_G, REAL *a, REAL *F, REAL *G, size_t M, size_t N, size_t O, size_t global_phi, int r, int theta, int phi){
 
 
     // Cada hilo copia el valor que le corresponde dentro del volumen por cada uno de los 4 tiempos
@@ -20,7 +20,7 @@ __device__ fillSharedMemory(REAL* sh_a, REAL* sh_F, REAL* sh_G, REAL *a, REAL *F
         }
     }
     // Right
-    if (r == M-1){
+    if (r == blockDim.x-1){
         for (size_t i = 0; i < 4 /*buffSize*/; i++) {
             sh_a[CI(i, threadIdx.z, threadIdx.y, threadIdx.x+blockDim.x)] = a[I(i, global_phi, theta, r+blockDim.x)];
             sh_F[CI(i, threadIdx.z, threadIdx.y, threadIdx.x+blockDim.x)] = F[I(i, global_phi, theta, r+blockDim.x)];
@@ -37,7 +37,7 @@ __device__ fillSharedMemory(REAL* sh_a, REAL* sh_F, REAL* sh_G, REAL *a, REAL *F
         }
     }
     // Back
-    if (theta == N-1){
+    if (theta == blockDim.y-1){
         for (size_t i = 0; i < 4 /*buffSize*/; i++) {
             sh_a[CI(i, threadIdx.z, threadIdx.y+blockDim.y, threadIdx.x)] = a[I(i, global_phi, theta+blockDim.y, r)];
             sh_F[CI(i, threadIdx.z, threadIdx.y+blockDim.y, threadIdx.x)] = F[I(i, global_phi, theta+blockDim.y, r)];
@@ -54,7 +54,7 @@ __device__ fillSharedMemory(REAL* sh_a, REAL* sh_F, REAL* sh_G, REAL *a, REAL *F
         }
     }
     // Bottom
-    if (phi == O){
+    if (phi == blockDim.z-1){
         for (size_t i = 0; i < 4 /*buffSize*/; i++) {
             sh_a[CI(i, threadIdx.z+blockDim.z, threadIdx.y, threadIdx.x)] = a[I(i, global_phi+blockDim.z, theta, r)];
             sh_F[CI(i, threadIdx.z+blockDim.z, threadIdx.y, threadIdx.x)] = F[I(i, global_phi+blockDim.z, theta, r)];
