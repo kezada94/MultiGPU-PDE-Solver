5df0b9544cf4bfede2d7441fe6b8f50cf790c302
Felipe Quezada <kezada66@gmail.com>
2021-06-18 03:29:44 -0400

    ds

--- gpu/src/defines.h
+++ gpu/src/defines.h
@@ -34,7 +34,7 @@
 #define GHOST_SIZE 2
 #define I(t, phi, theta, r) (t)*(O+GHOST_SIZE)*(N+GHOST_SIZE)*(M+GHOST_SIZE) + (phi+1)*(N+GHOST_SIZE)*(M+GHOST_SIZE) + (theta+1)*(M+GHOST_SIZE) + r+1
 #define E(t, phi, theta, r) (t)*(O+GHOST_SIZE)*(N+GHOST_SIZE)*(M+GHOST_SIZE) + (phi)*(N+GHOST_SIZE)*(M+GHOST_SIZE) + (theta)*(M+GHOST_SIZE) + r
-#define CI(t, phi, theta, r) (t)*(blockDim.z+GHOST_SIZE)*(blockDim.y+GHOST_SIZE)*(blockDim.x+GHOST_SIZE) + (phi+1)*(blockDim.y+GHOST_SIZE)*(blockDim.x+GHOST_SIZE) + (theta+1)*(blockDim.x+GHOST_SIZE) + r+1
+#define CI(func, t, phi, theta, r) (func)*4*(blockDim.z+GHOST_SIZE)*(blockDim.y+GHOST_SIZE)*(blockDim.x+GHOST_SIZE) + (t)*(blockDim.z+GHOST_SIZE)*(blockDim.y+GHOST_SIZE)*(blockDim.x+GHOST_SIZE) + (phi+1)*(blockDim.y+GHOST_SIZE)*(blockDim.x+GHOST_SIZE) + (theta+1)*(blockDim.x+GHOST_SIZE) + r+1
 
 
 #define E1 0.000
