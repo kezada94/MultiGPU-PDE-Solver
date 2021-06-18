a6f052e5594eed4052e63176bf1fc14f3d5f0ec8
Felipe Quezada <kezada66@gmail.com>
2021-06-18 03:38:20 -0400

    s

--- gpu/src/EquationF.cuh
+++ gpu/src/EquationF.cuh
@@ -7,6 +7,8 @@
  #include <fstream>
  #include <cmath>
  #include "defines.h"
+ #include "SharedMem.cuh"
+
  using namespace std;
 
 __global__ void computeNextF(REAL *a, REAL *F, REAL *G, size_t l, size_t tp1, size_t t, size_t tm1, size_t tm2, size_t M, size_t N, size_t O, size_t phi_offset, REAL dt, REAL dr, REAL dtheta, REAL dphi, REAL l_1, REAL l_2, REAL lamb, int p, int q, int L);
