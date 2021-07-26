4fe53c719b9a7c85988c8a15494f4da93e19ddf6
Felipe Quezada <kezada66@gmail.com>
2021-07-26 14:33:17 -0400

    fixed shared memory header

--- gpu/src/SharedMem.cuh
+++ gpu/src/SharedMem.cuh
@@ -12,7 +12,7 @@ __device__ inline void fillSharedMemory(REAL* sh, REAL *a, REAL *F, REAL *G, siz
     }
 }
 
-__device__ inline void copySharedMemoryToGlobal(REAL* sh, REAL *func, size_t M, size_t N, size_t O, size_t global_phi, int r, int theta, int phi, size_t tp1){
+__device__ inline void copySharedMemoryToGlobal(REAL* sh, REAL *func, int funcIndex, size_t M, size_t N, size_t O, size_t global_phi, int r, int theta, int phi, size_t tp1){
     // Cada hilo copia el valor que le corresponde dentro del volumen por cada uno de los 4 tiempos
-    func[I(tp1, phi, theta, r)] = sh[CI(0, tp1, threadIdx.z, threadIdx.y, threadIdx.x)];
+    func[E(tp1, phi, theta, r)] = sh[CI(funcIndex, tp1, threadIdx.z, threadIdx.y, threadIdx.x)];
 }
