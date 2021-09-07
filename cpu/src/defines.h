10565fa9176ba5333dffd2e0c515a68843b6e726
Felipe Quezada <kezada66@gmail.com>
2021-09-07 11:55:34 -0300

    working cpu version

--- cpu/src/defines.h
+++ cpu/src/defines.h
@@ -1,4 +1,7 @@
 #pragma once
+extern "C" {
+    #include "quadmath.h"
+}
 #include <iostream>
 #include <boost/multiprecision/float128.hpp>
 
@@ -8,12 +11,12 @@ using namespace boost::multiprecision;
 #define I(t, phi, theta, r) (t)*(O+GHOST_SIZE)*(N+GHOST_SIZE)*(M+GHOST_SIZE) + (phi+1)*(N+GHOST_SIZE)*(M+GHOST_SIZE) + (theta+1)*(M+GHOST_SIZE) + r+1
 #define E(t, phi, theta, r) (t)*(O)*(N)*(M) + (phi)*(N)*(M) + (theta)*(M) + r
 
-#define E1 0.000
-#define E2 0.000
-#define E3 0.000
-#define E4 0.000
-#define E5 0.000
-#define E6 0.000
+#define E1 0.00
+#define E2 0.00
+#define E3 0.00
+#define E4 0.00
+#define E5 0.00
+#define E6 0.00
 
 #define PI_1 E1*sin(dr*r)*sin(dtheta*theta)*sin(dphi*phi)
 #define PI_2 E2*sin(dr*r)*sin(dtheta*theta)*sin(dphi*phi)
@@ -22,6 +25,7 @@ using namespace boost::multiprecision;
 #define PI_5 E3*sin(dr*r)*sin(dtheta*theta)*sin(dphi*phi)
 #define PI_6 E3*sin(dr*r)*sin(dtheta*theta)*sin(dphi*phi)
 
-typedef float128 REAL;
+//typedef float128 REAL;
 //typedef cpp_dec_float_100 REAL;
+typedef long double REAL;
 //typedef double REAL;
