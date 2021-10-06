f8d86925d86e6bc40ce31fb63f6a4b195d7ed918
Felipe Quezada <kezada66@gmail.com>
2021-10-06 15:53:01 -0300

    d

--- cpu/src/EquationF.cpp
+++ cpu/src/EquationF.cpp
@@ -9,8 +9,8 @@
  #include "EquationF.h"
 
 REAL computeNextF(REAL *a, REAL *F, REAL *G, size_t t, size_t tm1, size_t tm2, size_t r, size_t theta, size_t phi, size_t M, size_t N, size_t O, REAL dt, REAL dr, REAL dtheta, REAL dphi, REAL l_1, REAL l_2, REAL lamb, int p, int q, int L){
-    return  ((dr*dr)*(dt*dt)*(F[I((t),(phi),(theta-1),(r))] - 2*F[I((t),(phi),(theta),(r))] + F[I((t),(phi),(theta+1),(r))]) - (dtheta*dtheta)*((dr*dr)*(F[I((tm1),(phi),(theta),(r))] - 2*F[I((t),(phi),(theta),(r))]) - (dt*dt)*(F[I((t),(phi),(theta),(r-1))] - 2*F[I((t),(phi),(theta),(r))] + F[I((t),(phi),(theta),(r+1))])))/((dr*dr)*(dtheta*dtheta));
+    return  (36*(dr*dr)*(dt*dt)*(F[I((t),(phi),(theta-1),(r))] - 2*F[I((t),(phi),(theta),(r))] + F[I((t),(phi),(theta+1),(r))]) - (dtheta*dtheta)*((dr*dr)*(F[I((tm1),(phi),(theta),(r))] - 2*F[I((t),(phi),(theta),(r))]) - 36*(dt*dt)*(F[I((t),(phi),(theta),(r-1))] - 2*F[I((t),(phi),(theta),(r))] + F[I((t),(phi),(theta),(r+1))])))/((dr*dr)*(dtheta*dtheta));
 }
 REAL computeFirstF(REAL *a, REAL *F, REAL *G, size_t t, size_t tm1, size_t tm2, size_t r, size_t theta, size_t phi, size_t M, size_t N, size_t O, REAL dt, REAL dr, REAL dtheta, REAL dphi, REAL l_1, REAL l_2, REAL lamb, int p, int q, int L){
-    return  ((dr*dr)*(dt*dt)*(F[I((t),(phi+1),(theta-1),(r))] - 2*F[I((t),(phi+1),(theta),(r))] + F[I((t),(phi+1),(theta+1),(r))]) + (dtheta*dtheta)*(F[I((t),(phi+1),(theta),(r))]*(dr*dr) + (dt*dt)*(F[I((t),(phi+1),(theta),(r-1))] - 2*F[I((t),(phi+1),(theta),(r))] + F[I((t),(phi+1),(theta),(r+1))])))/((dr*dr)*(dtheta*dtheta));
+    return  (36*(dr*dr)*(dt*dt)*(F[I((t),(phi+1),(theta-1),(r))] - 2*F[I((t),(phi+1),(theta),(r))] + F[I((t),(phi+1),(theta+1),(r))]) + (dtheta*dtheta)*(F[I((t),(phi+1),(theta),(r))]*(dr*dr) + 36*(dt*dt)*(F[I((t),(phi+1),(theta),(r-1))] - 2*F[I((t),(phi+1),(theta),(r))] + F[I((t),(phi+1),(theta),(r+1))])))/((dr*dr)*(dtheta*dtheta));
 }
--- cpu/src/EquationG.cpp
+++ cpu/src/EquationG.cpp
@@ -9,8 +9,8 @@
  #include "EquationG.h"
 
 REAL computeNextG(REAL *a, REAL *F, REAL *G, size_t t, size_t tm1, size_t tm2, size_t r, size_t theta, size_t phi, size_t M, size_t N, size_t O, REAL dt, REAL dr, REAL dtheta, REAL dphi, REAL l_1, REAL l_2, REAL lamb, int p, int q, int L){
-    return  ((dr*dr)*(dt*dt)*(G[I((t),(phi),(theta-1),(r))] - 2*G[I((t),(phi),(theta),(r))] + G[I((t),(phi),(theta+1),(r))]) - (dtheta*dtheta)*((dr*dr)*(G[I((tm1),(phi),(theta),(r))] - 2*G[I((t),(phi),(theta),(r))]) - (dt*dt)*(G[I((t),(phi),(theta),(r-1))] - 2*G[I((t),(phi),(theta),(r))] + G[I((t),(phi),(theta),(r+1))])))/((dr*dr)*(dtheta*dtheta));
+    return  (36*(dr*dr)*(dt*dt)*(G[I((t),(phi),(theta-1),(r))] - 2*G[I((t),(phi),(theta),(r))] + G[I((t),(phi),(theta+1),(r))]) - (dtheta*dtheta)*((dr*dr)*(G[I((tm1),(phi),(theta),(r))] - 2*G[I((t),(phi),(theta),(r))]) - 36*(dt*dt)*(G[I((t),(phi),(theta),(r-1))] - 2*G[I((t),(phi),(theta),(r))] + G[I((t),(phi),(theta),(r+1))])))/((dr*dr)*(dtheta*dtheta));
 }
 REAL computeFirstG(REAL *a, REAL *F, REAL *G, size_t t, size_t tm1, size_t tm2, size_t r, size_t theta, size_t phi, size_t M, size_t N, size_t O, REAL dt, REAL dr, REAL dtheta, REAL dphi, REAL l_1, REAL l_2, REAL lamb, int p, int q, int L){
-    return  ((dr*dr)*(dt*dt)*(G[I((t),(phi+1),(theta-1),(r))] - 2*G[I((t),(phi+1),(theta),(r))] + G[I((t),(phi+1),(theta+1),(r))]) + (dtheta*dtheta)*(G[I((t),(phi+1),(theta),(r))]*(dr*dr) + (dt*dt)*(G[I((t),(phi+1),(theta),(r-1))] - 2*G[I((t),(phi+1),(theta),(r))] + G[I((t),(phi+1),(theta),(r+1))])))/((dr*dr)*(dtheta*dtheta));
+    return  (36*(dr*dr)*(dt*dt)*(G[I((t),(phi+1),(theta-1),(r))] - 2*G[I((t),(phi+1),(theta),(r))] + G[I((t),(phi+1),(theta+1),(r))]) + (dtheta*dtheta)*(G[I((t),(phi+1),(theta),(r))]*(dr*dr) + 36*(dt*dt)*(G[I((t),(phi+1),(theta),(r-1))] - 2*G[I((t),(phi+1),(theta),(r))] + G[I((t),(phi+1),(theta),(r+1))])))/((dr*dr)*(dtheta*dtheta));
 }
